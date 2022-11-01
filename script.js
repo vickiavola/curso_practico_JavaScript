fc57eafc3edc88aba877c03c6281d137467a49e3
Vicki Avola <vickiavola7@gmail.com>
2022-11-01 05:32:52 +0100

    clase 10

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello, world');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafo = document.querySelector('.parrafo');
+// const parrafo = document.getElementsByClassName('.parrafo');
+const pid = document.querySelector('#pid');
+// const pid = document.getElementsById('#pid');
+const input = document.querySelector ('input');
+
+
+console.log({
+     h1,
+     p,
+     parrafo,
+     pid,
+     input,
+});
