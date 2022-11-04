88d7ad5f03e9d205182035617c4f60d9455ffc3b
Vicki Avola <vickiavola7@gmail.com>
2022-11-04 05:00:21 +0100

    Parte I

---
+++ clases_13/script.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener('click', btnOnClick);
+
+function btnOnClick() {
+     const input1Parse = parseInt(input1.value, 10);
+     const input2Parse = parseInt(input2.value, 10);
+     const sumaInputs = (input1Parse + input2Parse);
+     pResult.innerHTML = "Resultado: " + sumaInputs;
+};
+
+/*
+function btnOnClick() {
+     const sumaInputs = input1.value + input2.value;
+     pResult.innerText = "Resultado: " + sumaInputs;
+}
+*/
