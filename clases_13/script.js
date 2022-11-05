7244103692c1c9d48269d80a571a2f3bf1ac0383
Vicki Avola <vickiavola7@gmail.com>
2022-11-05 04:08:06 +0100

    Agregar el .gitignore

--- clases_13/script.js
+++ clases_13/script.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector('h1');
+const formulario = document.querySelector('#formulario');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-btn.addEventListener('click', btnOnClick);
+formulario.addEventListener('submit', sumarInputValues);
 
-function btnOnClick() {
+function sumarInputValues(event) {
+     event.preventDefault();
      const input1Parse = parseInt(input1.value, 10);
      const input2Parse = parseInt(input2.value, 10);
      const sumaInputs = (input1Parse + input2Parse);
@@ -14,7 +16,7 @@ function btnOnClick() {
 };
 
 /*
-function btnOnClick() {
+function sumarInputValues() {
      const sumaInputs = input1.value + input2.value;
      pResult.innerText = "Resultado: " + sumaInputs;
 }
